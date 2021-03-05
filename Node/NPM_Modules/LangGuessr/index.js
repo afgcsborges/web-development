4b9935b0df70c3b31d42f140cfd27b859bf3ecfe
Francisco Borges <afgcsborges@gmail.com>
2021-03-05 16:50:27 +0000

    creating an express app

--- Node/NPM_Modules/LangGuessr/index.js
+++ Node/NPM_Modules/LangGuessr/index.js
@@ -1,14 +1,20 @@
 // REMEMBER TO RUN "npm install" in your terminal (in the same directory as this file)
 const franc = require("franc");
 const langs = require("langs");
-const colors = require("colors")
+const colors = require("colors");
 
 const input = process.argv[2];
 const langCode = franc(input);
-if (langCode === 'und') {
-    console.log("SORRY, COULDN'T FIGURE IT OUT! TRY WITH MORE SAMPLE TEXT!".red)
+if (langCode === "und") {
+    console.log(
+        "SORRY, COULDN'T FIGURE IT OUT! TRY WITH MORE SAMPLE TEXT!".red
+    );
 } else {
+    console.log(langCode);
     const language = langs.where("3", langCode);
-    console.log(`Our best guess is: ${language.name}`.green)
+    if (!language) {
+        console.log(`Our best guess is: ${langCode}`.green);
+    } else {
+        console.log(`Our best guess is: ${language.name}`.green);
+    }
 }
-
