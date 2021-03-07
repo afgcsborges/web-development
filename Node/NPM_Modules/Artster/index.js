80b1c1b05b03f27626839858199f01a55d63e646
Francisco Borges <afgcsborges@gmail.com>
2021-03-07 21:34:36 +0000

    playing with artster

--- Node/NPM_Modules/Artster/index.js
+++ Node/NPM_Modules/Artster/index.js
@@ -3,7 +3,7 @@ const figlet = require("figlet");
 const colors = require("colors");
 figlet("Hello World!!", function (err, data) {
     if (err) {
-        console.log("Something went wrong...");
+        console.log("Something went terribly wrong...");
         console.dir(err);
         return;
     }
