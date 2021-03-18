a40fa5cb0be03ddd98c321a6702552ed70bc72f4
Francisco Borges <afgcsborges@gmail.com>
2021-03-18 22:22:34 +0000

    dode review

--- Session_And_Flash/SessionDemo/index.js
+++ Session_And_Flash/SessionDemo/index.js
@@ -1,30 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const session = require('express-session');
+const session = require("express-session");
 
-const sessionOptions = { secret: 'thisisnotagoodsecret', resave: false, saveUninitialized: false }
+const sessionOptions = { secret: "thisisnotagoodsecret", resave: false, saveUninitialized: false };
 app.use(session(sessionOptions));
 
-app.get('/viewcount', (req, res) => {
+app.get("/viewcount", (req, res) => {
     if (req.session.count) {
         req.session.count += 1;
     } else {
         req.session.count = 1;
     }
-    res.send(`You have viewed this page ${req.session.count} times`)
-})
+    res.send(`You have viewed this page ${req.session.count} times`);
+});
 
-app.get('/register', (req, res) => {
-    const { username = 'Anonymous' } = req.query;
+app.get("/register", (req, res) => {
+    const { username = "Anonymous" } = req.query;
     req.session.username = username;
-    res.redirect('/greet')
-})
+    res.redirect("/greet");
+});
 
-app.get('/greet', (req, res) => {
+app.get("/greet", (req, res) => {
     const { username } = req.session;
-    res.send(`Welcome back, ${username}`)
-})
+    res.send(`Welcome back, ${username}`);
+});
 
 app.listen(3000, () => {
-    console.log('listening on port 3000')
-})
+    console.log("listening on port 3000");
+});
